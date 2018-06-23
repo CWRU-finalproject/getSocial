d738ee2757c1b38a2c781e6dc2c5dde780c5a341
Matt Pedicini <mattpedicini@Matts-MacBook-Air.local>
2018-06-23 13:22:27 -0400

    Added proxy to package.json added dependencies and server listening to server.js. Server runs but i dont know if it will connect api to the react app

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser')
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 8080
+app.use(express.static(path.join(__dirname, 'client/public')));
+
+app.get('/ping', function (req, res) {
+ return res.send('pong');
+});
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(PORT, function(){
+    console.log(`Server running on ${PORT}`)
+});
