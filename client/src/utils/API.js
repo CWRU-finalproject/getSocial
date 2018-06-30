6bbfe5516bbc83e830fa8a62ea0c1fe805a879d0
John <thehumantorch88@gmail.com>
2018-06-30 11:27:26 -0400

    user sign up/ posting to api and database

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -2,30 +2,34 @@ import axios from "axios"
 
 export default {
 //----- Need to change from AJAX to AXIOS -----//
-getUser: function(email,ps) {
-    $.ajax({
-        url: "/api/user",
-        type: "GET",
-        data: {
-            email:email,
-            password:ps
-        }
-    }).then((user) => {
-        return user;
-    }).catch((err) => {
-        console.log(err);
-    })
-},
-postUser: function(obj){
-    $.ajax({
-        url:"/api/user",
-        type:"POST",
-        data: obj
-    }).then(() => {
-        alert("succesful")
-    }).catch((err) => {
-        alert(err);
-    })
+// getUser: function(email,ps) {
+//     $.ajax({
+//         url: "/api/user",
+//         type: "GET",
+//         data: {
+//             email:email,
+//             password:ps
+//         }
+//     }).then((user) => {
+//         return user;
+//     }).catch((err) => {
+//         console.log(err);
+//     })
+
+
+// },
+postUser: function(userObj){
+    return axios.post("/api/user", userObj);
+
+    // $.ajax({
+    //     url:"/api/user",
+    //     type:"POST",
+    //     data: obj
+    // }).then(() => {
+    //     alert("succesful")
+    // }).catch((err) => {
+    //     alert(err);
+    // })
 }
     
 }
